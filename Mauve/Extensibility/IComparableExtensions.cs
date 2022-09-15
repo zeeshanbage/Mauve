b72e80b4a37f991b3be4d91acdce8b5295d8eba3
tacosontitan <jdavis@globallcs.com>
2022-09-15 07:49:13 -0500

    Added IComparable extensions.

---
+++ Mauve/Extensibility/IComparableExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mauve.Core.Extensibility
+{
+    /// <summary>
+    /// Represents a collection of extension methods for <see cref="IComparable"/> instances.
+    /// </summary>
+    public static class IComparableExtensions
+    {
+
+    }
+}
