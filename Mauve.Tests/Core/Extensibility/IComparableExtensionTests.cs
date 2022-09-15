9b13ef8ba279319f6e2bbe70f732708db5fd2216
tacosontitan <jdavis@globallcs.com>
2022-09-15 17:03:59 -0500

    Created test methods.

---
+++ Mauve.Tests/Core/Extensibility/IComparableExtensionTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Mauve.Extensibility;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mauve.Tests.Core.Extensibility
+{
+    [TestClass]
+    public class IComparableExtensionTests
+    {
+        #region Constants
+
+        private const int LowerBound = 1;
+        private const int UpperBound = 10;
+
+        #endregion
+
+        #region Tests
+
+        [TestMethod()] public void RangeExceeded() => Assert.IsFalse(15.WithinRange(LowerBound, UpperBound));
+        [TestMethod()] public void WithinRange() => Assert.IsTrue(3.WithinRange(LowerBound, UpperBound));
+
+        #endregion
+    }
+}
