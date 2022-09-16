e062ff5459106ba232b9d731f3b5b25860fd2c04
tacosontitan <jdavis@globallcs.com>
2022-09-15 20:29:59 -0500

    Created initial substring queue test.

--- Mauve.Tests/Core/Text/SubstringQueueTests.cs
+++ Mauve.Tests/Core/Text/SubstringQueueTests.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Mauve.Text;
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Mauve.Tests.Core.Text
@@ -14,11 +16,50 @@ namespace Mauve.Tests.Core.Text
 
         #region Constants
 
-        public const string TestString = "this23true3.14159test:09152022";
+        public const string TestString = "this23true3.14159test:09/15/2022";
 
         #endregion
 
         #region Tests
+
+        [TestMethod("Full Parse")]
+        public void FullParse()
+        {
+            var queue = new SubstringQueue(TestString);
+            
+            try
+            {
+                // Parse the first string.
+                _ = queue.Next(4, out string firstString);
+                Assert.AreEqual("this", firstString);
+
+                // Parse the integer.
+                _ = queue.Next(2, out int integer);
+                Assert.AreEqual(23, integer);
+
+                // Parse the boolean.
+                _ = queue.Next(4, out bool boolean);
+                Assert.AreEqual(true, boolean);
+
+                // Parse PI.
+                _ = queue.Next(7, out double pi);
+                Assert.AreEqual(3.14159, pi);
+
+                // Parse the second string.
+                _ = queue.Next(4, out string secondString);
+                Assert.AreEqual("test", secondString);
+
+                // Skip the next character, this test doesn't use it.
+                queue.Skip(1);
+
+                // Parse the date using remaining.
+                queue.Remainder(out DateTime date);
+                Assert.AreEqual(new DateTime(2022, 9, 15), date);
+            } catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
 
         #endregion
 
