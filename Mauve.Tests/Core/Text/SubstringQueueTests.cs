71824f585946325810f997bd44416c73e8ebdc60
tacosontitan <jdavis@globallcs.com>
2022-09-15 19:14:12 -0500

    Created substring queue test class.

---
+++ Mauve.Tests/Core/Text/SubstringQueueTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mauve.Tests.Core.Text
+{
+    [TestClass]
+    public class SubstringQueueTests
+    {
+
+        #region Constants
+
+        public const string TestString = "this23true3.14159test:09152022";
+
+        #endregion
+
+        #region Tests
+
+        #endregion
+
+    }
+}
