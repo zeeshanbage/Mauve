ebe8839a85d0a40ca120dce4b738fb577e4e7d5a
tacosontitan <jdavis@globallcs.com>
2022-09-19 17:21:44 -0500

    Added type argument, created method signatures.

--- Mauve/Patterns/Mediator/IMediator.cs
+++ Mauve/Patterns/Mediator/IMediator.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Mauve.Patterns
 {
-    public interface IMediator
+    public interface IMediator<T>
     {
+        void Send(T data, IMediatorClient<T> recipient);
+        void Send(T data, IEnumerable<IMediatorClient<T>> recipients);
+        Task SendAsync(T data, IMediatorClient<T> recipients);
+        Task SendAsync(T data, IEnumerable<IMediatorClient<T>> recipients);
     }
 }
