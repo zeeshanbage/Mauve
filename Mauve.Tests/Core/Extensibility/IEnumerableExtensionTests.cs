b3ad8f8d759faf00c077f0b9812c4f154fcff142
tacosontitan <jdavis@globallcs.com>
2022-09-19 17:50:08 -0500

    Created write all test.

---
+++ Mauve.Tests/Core/Extensibility/IEnumerableExtensionTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Mauve.Extensibility;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mauve.Tests.Core.Extensibility
+{
+    [TestClass]
+    public class IEnumerableExtensionTests
+    {
+        [TestMethod("Write Each")]
+        public void WriteEach()
+        {
+            var guids = new Guid[100];
+            for (int i = 0; i < 100; i++)
+                guids[i] = Guid.NewGuid();
+
+            try
+            {
+                guids.ForEach(guid => Console.WriteLine(guid));
+            } catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+    }
+}
