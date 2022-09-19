c33fdbbdb0b889cb1249c22edaab66750cbaf194
tacosontitan <jdavis@globallcs.com>
2022-09-19 17:31:50 -0500

    Created ForEach extension for IEnumerable.

---
+++ Mauve/Extensibility/IEnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Mauve.Extensibility
+{
+    public static class IEnumerableExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> collection, Action<T> action)
+        {
+            foreach (T item in collection)
+                action(item);
+        }
+    }
+}
