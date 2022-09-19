d71ca44fa941a9e71b1212c2d197ec4ebb21f491
Arkadiusz Surowka <95arek95@gmail.com>
2022-09-19 22:01:17 +0200

    Added extension for string.Equals to simplify ignoring case and test for that extension

---
+++ Mauve.Tests/Core/Extensibility/StringExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿using Mauve.Extensibility;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mauve.Tests.Core.Extensibility
+{
+    [TestClass]
+    public class StringExtensionsTests
+    {
+        [TestMethod]
+        [DataRow("this-is-a-test", "THIS-IS-A-TEST", false, false)]
+        [DataRow("this-is-a-test", "THIS-IS-A-TEST", true, true)]
+        public void EqualsTest(string first, string second, bool ignoreCase, bool expected)
+        {
+            var result = first.Equals(second, ignoreCase);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Mauve/Extensibility/StringExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Mauve.Extensibility
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Checks whether this string and an input string have the same value. A parameter specifies if case should be ignored.
+        /// </summary>
+        /// <param name="firstString"></param>
+        /// <param name="secondString"></param>
+        /// <param name="ignoreCase"></param>
+        /// <returns></returns>
+        public static bool Equals(this string firstString, string secondString, bool ignoreCase)
+        {
+            if(ignoreCase)
+            {
+                return firstString.Equals(secondString, StringComparison.InvariantCultureIgnoreCase);
+            }
+            else
+            {
+                return firstString.Equals(secondString, StringComparison.InvariantCulture);
+            }
+        }
+    }
+}
