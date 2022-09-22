8aae928e6fcb7c423f2ce254063d48b7b4352bbb
tacosontitan <jkdavis1691@gmail.com>
2022-09-22 14:39:30 -0500

    Added date time extensions.

---
+++ Mauve/Extensibility/DateTimeExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Mauve.Extensibility
+{
+    public static class DateTimeExtensions
+    {
+        public static string Format(this DateTime input, DateFormat format) => string.Empty;
+    }
+}
