2e740b1bd5dc7375181e5871d512a3f1c28d94e4
tacosontitan <jkdavis1691@gmail.com>
2022-09-22 17:58:22 -0500

    Updated documentation, simplified logic.

--- Mauve/Extensibility/StringExtensions.cs
+++ Mauve/Extensibility/StringExtensions.cs
@@ -7,19 +7,19 @@ namespace Mauve.Extensibility
         /// <summary>
         /// Checks whether this string and an input string have the same value. A parameter specifies if case should be ignored.
         /// </summary>
-        /// <param name="firstString"></param>
-        /// <param name="secondString"></param>
-        /// <param name="ignoreCase"></param>
-        /// <returns></returns>
-        public static bool Equals(this string firstString, string secondString, bool ignoreCase)
+        /// <param name="input">The input string used to check equality with.</param>
+        /// <param name="target">The target of the equality check.</param>
+        /// <param name="ignoreCase">Should character case be ignored?</param>
+        /// <returns>Returns <see langword="true"/> if the <paramref name="input"/> and <paramref name="target"/> are equal, with respect to the <paramref name="ignoreCase"/> specification, otherwise <see langword="false"/>.</returns>
+        public static bool Equals(this string input, string target, bool ignoreCase)
         {
-            if (ignoreCase)
-            {
-                return firstString.Equals(secondString, StringComparison.InvariantCultureIgnoreCase);
-            } else
-            {
-                return firstString.Equals(secondString, StringComparison.InvariantCulture);
-            }
+            // Create a comparison type based on whether or not we should ignore case.
+            StringComparison comparisonType = ignoreCase ?
+                StringComparison.InvariantCultureIgnoreCase :
+                StringComparison.InvariantCulture;
+
+            // Return whether or not the input string is equal to the comparison string, with respect to the comparison type.
+            return input.Equals(target, comparisonType);
         }
     }
 }
