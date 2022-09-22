e6d0b878b03c0b37594d965292cece88edbfc5a0
tacosontitan <jkdavis1691@gmail.com>
2022-09-22 15:04:53 -0500

    Added universal overload.

--- Mauve/Extensibility/DateTimeExtensions.cs
+++ Mauve/Extensibility/DateTimeExtensions.cs
@@ -17,6 +17,15 @@ namespace Mauve.Extensibility
         /// <param name="format">The <see cref="DateFormat"/> to translate to.</param>
         /// <returns>Returns the specified <see cref="DateTime"/> instance translated to a <see cref="string"/> using the specified <see cref="DateFormat"/>.</returns>
         public static string Format(this DateTime input, DateFormat format) => input.ToString(GetFormatSpecifier(format));
+        /// <summary>
+        /// Translates the specified <see cref="DateTime"/> instance to a string using a specified <see cref="DateFormat"/>.
+        /// </summary>
+        /// <param name="input">The <see cref="DateTime"/> instance to translate.</param>
+        /// <param name="format">The <see cref="DateFormat"/> to translate to.</param>
+        /// <param name="universal">Specifies whether or not <see cref="DateTime.ToUniversalTime()"/> is invoked prior to formatting.</param>
+        /// <returns>Returns the specified <see cref="DateTime"/> instance translated to a <see cref="string"/> using the specified <see cref="DateFormat"/>.</returns>
+        public static string Format(this DateTime input, DateFormat format, bool universal) =>
+            universal ? input.ToUniversalTime().Format(format) : input.Format(format);
 
         #endregion
 
