f44aaf218ff79e700d43114ac41660d470ce5507
tacosontitan <jdavis@globallcs.com>
2022-09-22 06:27:29 -0500

    Added alias attribute.

---
+++ Mauve/AliasAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Mauve
+{
+    /// <summary>
+    /// An <see cref="Attribute"/> designed for aliasing objects and their members.
+    /// </summary>
+    public class AliasAttribute : Attribute
+    {
+        /// <summary>
+        /// The alias assigned to the object or member this attribute is applied to.
+        /// </summary>
+        public string Value { get; set; }
+        /// <summary>
+        /// Creates a new <see cref="AliasAttribute"/> instance with the specified alias.
+        /// </summary>
+        /// <param name="alias">The alias assigned to the object or member this attribute is applied to.</param>
+        public AliasAttribute(string value) => Value = value;
+    }
+}
