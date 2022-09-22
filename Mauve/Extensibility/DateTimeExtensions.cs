9054d78bd1fd57c3a0b7b6582ea6e24eda1cc2ea
tacosontitan <jkdavis1691@gmail.com>
2022-09-22 14:54:22 -0500

    Added region, added summary tags.

--- Mauve/Extensibility/DateTimeExtensions.cs
+++ Mauve/Extensibility/DateTimeExtensions.cs
@@ -7,6 +7,18 @@ namespace Mauve.Extensibility
     /// </summary>
     public static class DateTimeExtensions
     {
+
+        #region Public Methods
+
+        /// <summary>
+        /// Translates the specified <see cref="DateTime"/> instance to a string using a specified <see cref="DateFormat"/>.
+        /// </summary>
+        /// <param name="input">The <see cref="DateTime"/> instance to translate.</param>
+        /// <param name="format">The <see cref="DateFormat"/> to translate to.</param>
+        /// <returns>Returns the specified <see cref="DateTime"/> instance translated to a <see cref="string"/> using the specified <see cref="DateFormat"/>.</returns>
         public static string Format(this DateTime input, DateFormat format) => string.Empty;
+
+        #endregion
+
     }
 }
