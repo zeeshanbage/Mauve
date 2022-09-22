67abff567ef1c1a2299bfbf116876f314adcdb60
tacosontitan <jkdavis1691@gmail.com>
2022-09-22 14:51:41 -0500

    Added summary tags to class.

--- Mauve/Extensibility/DateTimeExtensions.cs
+++ Mauve/Extensibility/DateTimeExtensions.cs
@@ -2,6 +2,9 @@
 
 namespace Mauve.Extensibility
 {
+    /// <summary>
+    /// Represents a collection of extension methods for <see cref="DateTime"/> instances.
+    /// </summary>
     public static class DateTimeExtensions
     {
         public static string Format(this DateTime input, DateFormat format) => string.Empty;
