7dc183a59125145761865effbe478d8918030aae
tacosontitan <jkdavis1691@gmail.com>
2022-09-22 14:40:03 -0500

    Added date format

---
+++ Mauve/DateFormat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Mauve.Extensibility;
+
+namespace Mauve
+{
+    /// <summary>
+    /// Represents a <see cref="DateFormat"/> for creating <see cref="string"/> representations of <see cref="DateTime"/> instances.
+    /// </summary>
+    public enum DateFormat
+    {
+        /// <summary>
+        /// No format specified.
+        /// </summary>
+        /// <remarks>Calls to <see cref="DateTimeExtensions.Format(DateTime, DateFormat)"/> will simply invoke <see cref="DateTime.ToString()"/>.</remarks>
+        None = 0
+    }
+}
