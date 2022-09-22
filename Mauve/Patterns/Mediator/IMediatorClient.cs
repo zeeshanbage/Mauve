02fa88c6a2f6845523d11ccc50e73c91b1edc893
tacosontitan <jkdavis1691@gmail.com>
2022-09-22 17:52:34 -0500

    Added empty summary tags.

--- Mauve/Patterns/Mediator/IMediatorClient.cs
+++ Mauve/Patterns/Mediator/IMediatorClient.cs
@@ -2,9 +2,22 @@
 
 namespace Mauve.Patterns
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface IMediatorClient<T>
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="data"></param>
         void HandleIncomingData(T data);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
         Task HandleIncomingDataAsync(T data);
     }
 }
