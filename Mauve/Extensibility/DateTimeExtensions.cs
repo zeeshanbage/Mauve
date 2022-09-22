2434e8b25a9dfbac6d1acbf013363fe70113fba5
tacosontitan <jkdavis1691@gmail.com>
2022-09-22 14:55:07 -0500

    Added GetFormatSpecifier

--- Mauve/Extensibility/DateTimeExtensions.cs
+++ Mauve/Extensibility/DateTimeExtensions.cs
@@ -16,7 +16,13 @@ namespace Mauve.Extensibility
         /// <param name="input">The <see cref="DateTime"/> instance to translate.</param>
         /// <param name="format">The <see cref="DateFormat"/> to translate to.</param>
         /// <returns>Returns the specified <see cref="DateTime"/> instance translated to a <see cref="string"/> using the specified <see cref="DateFormat"/>.</returns>
-        public static string Format(this DateTime input, DateFormat format) => string.Empty;
+        public static string Format(this DateTime input, DateFormat format) => input.ToString(GetFormatSpecifier(format));
+
+        #endregion
+
+        #region Private Methods
+
+        private static string GetFormatSpecifier(DateFormat format) => string.Empty;
 
         #endregion
 
