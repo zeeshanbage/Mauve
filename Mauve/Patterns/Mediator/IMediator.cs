4eaa4b4152c4dca59c4e7aad53d60931e6c839c3
tacosontitan <jkdavis1691@gmail.com>
2022-09-22 17:52:15 -0500

    Added empty summary tags.

--- Mauve/Patterns/Mediator/IMediator.cs
+++ Mauve/Patterns/Mediator/IMediator.cs
@@ -3,11 +3,37 @@ using System.Threading.Tasks;
 
 namespace Mauve.Patterns
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface IMediator<T>
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="recipient"></param>
         void Send(T data, IMediatorClient<T> recipient);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="recipients"></param>
         void Send(T data, IEnumerable<IMediatorClient<T>> recipients);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="recipients"></param>
+        /// <returns></returns>
         Task SendAsync(T data, IMediatorClient<T> recipients);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="recipients"></param>
+        /// <returns></returns>
         Task SendAsync(T data, IEnumerable<IMediatorClient<T>> recipients);
     }
 }
