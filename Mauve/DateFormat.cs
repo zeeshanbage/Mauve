491d0c7eca6a12f7f00a83b177b1ee1439b4d2e1
tacosontitan <jkdavis1691@gmail.com>
2022-09-22 14:40:30 -0500

    Added iso8601

--- Mauve/DateFormat.cs
+++ Mauve/DateFormat.cs
@@ -13,6 +13,12 @@ namespace Mauve
         /// No format specified.
         /// </summary>
         /// <remarks>Calls to <see cref="DateTimeExtensions.Format(DateTime, DateFormat)"/> will simply invoke <see cref="DateTime.ToString()"/>.</remarks>
-        None = 0
+        None = 0,
+        /// <summary>
+        /// ISO 8601 is an international standard covering the worldwide exchange and communication of date and time-related data.
+        /// </summary>
+        /// <remarks>Calls to <see cref="DateTimeExtensions.Format(DateTime, DateFormat)"/> will use the format: <c>yyyy-MM-ddTHH:mm:ss.ffK</c>.</remarks>
+        /// <see href="https://en.wikipedia.org/wiki/ISO_8601"/>
+        Iso8601 = 1
     }
 }
