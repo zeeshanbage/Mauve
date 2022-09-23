ea8ab54cec3e1e4d0edcfb83b6624adacbec0707
tacosontitan <jkdavis1691@gmail.com>
2022-09-23 16:31:33 -0500

    Added unit tests for Rijndael cryptography service.

---
+++ Mauve.Tests/Core/Security/RijndaelServiceTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Mauve.Security;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mauve.Tests.Core.Security
+{
+    [TestClass]
+    public class RijndaelServiceTests
+    {
+        [TestMethod]
+        [DataRow("this is a test", false)]
+        public void Encryption(string input, bool expectedResult)
+        {
+            try
+            {
+                using (var rijndael = new RijndaelCryptographyService())
+                {
+                    string encryptionResult = rijndael.Encrypt(input);
+                    bool result = input.Equals(encryptionResult);
+                    Assert.AreEqual(expectedResult, result);
+                }
+            } catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+        [TestMethod]
+        [DataRow("this is a test", true)]
+        public void Decryption(string input, bool expectedResult)
+        {
+            try
+            {
+                using (var rijndael = new RijndaelCryptographyService())
+                {
+                    string encryptionResult = rijndael.Encrypt(input);
+                    string decryptionResult = rijndael.Decrypt<string>(encryptionResult);
+                    bool result = input.Equals(decryptionResult);
+                    Assert.AreEqual(expectedResult, result);
+                }
+            } catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+    }
+}
