db9bf2c496de4158a3e8eb6bfed5ed31c1b28649
tacosontitan <jkdavis1691@gmail.com>
2022-09-23 16:14:09 -0500

    Added cryptography service.

---
+++ Mauve/Security/CryptographyService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Mauve.Security
+{
+    /// <summary>
+    /// Represents a <see cref="CryptographyService"/> to be utilized for simplified encryption and decryption of data.
+    /// </summary>
+    public abstract class CryptographyService : IDisposable
+    {
+
+        #region Public Methods
+
+        /// <summary>
+        /// Disposes of the <see cref="CryptographyService"/> instance and its resources.
+        /// </summary>
+        public abstract void Dispose();
+        /// <summary>
+        /// Encrypts the specified data based on the current configuration.
+        /// </summary>
+        /// <typeparam name="T">The type of the data being encrypted.</typeparam>
+        /// <param name="input">The data to encrypt.</param>
+        /// <returns>Returns the data encrypted and encoded using the current configuration.</returns>
+        public abstract string Encrypt<T>(T input);
+        /// <summary>
+        /// Decrypts the specified data based on the current configuration.
+        /// </summary>
+        /// <typeparam name="T">The type of the data being decrypted.</typeparam>
+        /// <param name="input">The data to decrypt.</param>
+        /// <returns>Returns the data decrypted using the current configuration.</returns>
+        public abstract T Decrypt<T>(string input);
+
+        #endregion
+
+    }
+}
