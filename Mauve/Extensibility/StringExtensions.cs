b1391f88c585c2048ab2e2ad3044d2a427183946
tacosontitan <jkdavis1691@gmail.com>
2022-09-24 14:39:28 -0500

    Cleaned up documentation, made contains consistent with equals.

--- Mauve/Extensibility/StringExtensions.cs
+++ Mauve/Extensibility/StringExtensions.cs
@@ -7,23 +7,21 @@ namespace Mauve.Extensibility
     public static class StringExtensions
     {
         /// <summary>
-        /// Checks whether this string and an input string contain the same value. A parameter specifies if case should be ignored.
+        /// Checks whether this string contains an input string. A parameter specifies if case should be ignored.
         /// </summary>
-        /// <param name="firstString"></param>
-        /// <param name="secondString"></param>
-        /// <param name="ignoreCase"></param>
-        /// <returns></returns>
-        public static bool Contains(this string firstString, string secondString, bool ignoreCase)
+        /// <param name="input">The input string used to check.</param>
+        /// <param name="target">The target to search for within the input string.</param>
+        /// <param name="ignoreCase">Should character case be ignored?</param>
+        /// <returns>Returns <see langword="true"/> if the <c>input</c> contains the <c>target</c>, with respect to the <c>ignoreCase</c> specification, otherwise <see langword="false"/>.</returns>
+        public static bool Contains(this string input, string target, bool ignoreCase)
         {
-            if (ignoreCase)
-            {
-                // If ignoreCase is set to true, we'll ignore the case
-                return firstString.IndexOf(secondString, StringComparison.InvariantCultureIgnoreCase) >= 0;
-            } else
-            {
-                // If ignoreCase is false/not set, we won't ignore the case
-                return firstString.IndexOf(secondString, StringComparison.InvariantCulture) >= 0;
-            }
+            // Create a comparison type based on whether or not we should ignore case.
+            StringComparison comparisonType = ignoreCase ?
+                StringComparison.InvariantCultureIgnoreCase :
+                StringComparison.InvariantCulture;
+
+            // Return whether or not the input string contains comparison string, with respect to the comparison type.
+            return input?.IndexOf(target, comparisonType) >= 0;
         }
         /// <summary>
         /// Deserializes the specified input utilizing the specified <see cref="SerializationMethod"/>.
@@ -52,7 +50,7 @@ namespace Mauve.Extensibility
         /// <param name="input">The input string used to check equality with.</param>
         /// <param name="target">The target of the equality check.</param>
         /// <param name="ignoreCase">Should character case be ignored?</param>
-        /// <returns>Returns <see langword="true"/> if the <paramref name="input"/> and <paramref name="target"/> are equal, with respect to the <paramref name="ignoreCase"/> specification, otherwise <see langword="false"/>.</returns>
+        /// <returns>Returns <see langword="true"/> if the <c>input</c> and <c>target</c>, are equal, with respect to the <c>ignoreCase</c> specification, otherwise <see langword="false"/>.</returns>
         public static bool Equals(this string input, string target, bool ignoreCase)
         {
             // Create a comparison type based on whether or not we should ignore case.
