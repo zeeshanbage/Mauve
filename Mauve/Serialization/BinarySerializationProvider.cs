5e07322178946648ee18a96bf0ddad698aeef937
tacosontitan <jkdavis1691@gmail.com>
2022-09-24 10:40:51 -0500

    Created a basic and untested binary serialization provider.

---
+++ Mauve/Serialization/BinarySerializationProvider.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace Mauve.Serialization
+{
+    /// <summary>
+    /// Represents a <see cref="SerializationProvider"/> focused on serializing and deserializing data using <see cref="SerializationMethod.Binary"/>.
+    /// </summary>
+    public class BinarySerializationProvider : SerializationProvider
+    {
+
+        #region Properties
+
+        public Encoding Encoding { get; private set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Creates a new instance of <see cref="BinarySerializationProvider"/> using <see cref="System.Text.Encoding.Unicode"/>.
+        /// </summary>
+        public BinarySerializationProvider() : this(Encoding.Unicode) { }
+        /// <summary>
+        /// Creates a new instance of <see cref="BinarySerializationProvider"/> using the specified <see cref="System.Text.Encoding"/>.
+        /// </summary>
+        public BinarySerializationProvider(Encoding encoding) :
+            base(SerializationMethod.Binary) =>
+                Encoding = encoding;
+
+        #endregion
+
+        #region Public Methods
+
+        public override T Deserialize<T>(string input)
+        {
+            byte[] decodedInput = Encoding.GetBytes(input);
+            using (var stream = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                stream.Write(decodedInput, 0, decodedInput.Length);
+                _ = stream.Seek(0, SeekOrigin.Begin);
+                object deserializedObject = formatter.Deserialize(stream);
+                return (T)Convert.ChangeType(deserializedObject, typeof(T));
+            }
+        }
+        public override string Serialize<T>(T input)
+        {
+            byte[] serializedBytes = null;
+            var formatter = new BinaryFormatter();
+            using (var stream = new MemoryStream())
+            {
+                formatter.Serialize(stream, input);
+                serializedBytes = stream.ToArray();
+            }
+
+            return Encoding.GetString(serializedBytes);
+        }
+
+        #endregion
+
+    }
+}
