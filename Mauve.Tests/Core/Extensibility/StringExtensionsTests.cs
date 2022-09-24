c62d27044bfe2247d74c881c07cffb9c5b390ce8
tacosontitan <jkdavis1691@gmail.com>
2022-09-24 14:45:28 -0500

    Added test cases for null handling.

--- Mauve.Tests/Core/Extensibility/StringExtensionsTests.cs
+++ Mauve.Tests/Core/Extensibility/StringExtensionsTests.cs
@@ -10,19 +10,23 @@ namespace Mauve.Tests.Core.Extensibility
         [TestMethod]
         [DataRow("this-is-a-test", "THIS-IS-A-TEST", false, false)]
         [DataRow("this-is-a-test", "THIS-IS-A-TEST", true, true)]
+        [DataRow("this-is-a-test", null, true, false)]
+        [DataRow(null, "THIS-IS-A-TEST", true, false)]
+        [DataRow(null, null, true, false)]
         public void EqualsTest(string first, string second, bool ignoreCase, bool expected)
         {
-            var result = first.Equals(second, ignoreCase);
-
+            bool result = first.Equals(second, ignoreCase);
             Assert.AreEqual(expected, result);
         }
         [TestMethod]
         [DataRow("this-is-a-test", "TEST", false, false)]
         [DataRow("this-is-a-test", "TEST", true, true)]
+        [DataRow("this-is-a-test", null, true, false)]
+        [DataRow(null, "THIS-IS-A-TEST", true, false)]
+        [DataRow(null, null, true, false)]
         public void ContainsTest(string first, string second, bool ignoreCase, bool expected)
         {
-            var result = first.Contains(second, ignoreCase);
-
+            bool result = first.Contains(second, ignoreCase);
             Assert.AreEqual(expected, result);
         }
     }
