f10405bff3f32d4ded80aa94512b733ea8e883d8
tacosontitan <jkdavis1691@gmail.com>
2022-09-24 10:47:35 -0500

    Added serialize method.

--- Mauve/Extensibility/GenericExtensions.cs
+++ Mauve/Extensibility/GenericExtensions.cs
@@ -1,15 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using Mauve.Serialization;
 
 namespace Mauve.Extensibility
 {
+    /// <summary>
+    /// Represents a collection of extension methods that are applicable to all types utilizing generics for type safety.
+    /// </summary>
     public static class GenericExtensions
     {
 
         #region Public Methods
+
+        /// <summary>
+        /// Serializes the current state of the specified input utilizing the specified <see cref="SerializationMethod"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the data to be serialized.</typeparam>
+        /// <param name="input">The data to be serialized.</param>
+        /// <param name="serializationMethod">The <see cref="Mauve.SerializationMethod"/> that should be utilized for serialization.</param>
+        /// <returns>Returns the input data serialized using the specified <see cref="Mauve.SerializationMethod"/>.</returns>
+        public static string Serialize<T>(T input, SerializationMethod serializationMethod)
+        {
+            SerializationProvider serializationProvider;
+            switch (serializationMethod)
+            {
+                case SerializationMethod.Binary: serializationProvider = new BinarySerializationProvider(); break;
+                case SerializationMethod.Xml: serializationProvider = new XmlSerializationProvider(); break;
+                case SerializationMethod.Json: serializationProvider = new JsonSerializationProvider(); break;
+                case SerializationMethod.Yaml: serializationProvider = null; break;
+                default: serializationProvider = new RawSerializationProvider(); break;
+            }
+
+            return serializationProvider.Serialize(input);
+        }
 
         #endregion
 
