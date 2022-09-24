d3f6ab91575f3d55007d52deaacd5fadfde913fa
tacosontitan <jkdavis1691@gmail.com>
2022-09-24 16:06:42 -0500

    Added binary serialization test shell.

---
+++ Mauve.Tests/Core/Serialization/BinarySerializationTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mauve.Tests.Core.Serialization
+{
+    [TestClass]
+    public class BinarySerializationTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
