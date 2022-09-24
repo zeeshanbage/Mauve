7bc737de41d3cae84bff406e97db698f292e8954
tacosontitan <jkdavis1691@gmail.com>
2022-09-24 10:26:26 -0500

    Added basic json serialization provider.

---
+++ Mauve/Serialization/JsonSerializationProvider.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+
+namespace Mauve.Serialization
+{
+    /// <summary>
+    /// Represents a <see cref="SerializationProvider"/> focused on serializing and deserializing data using <see cref="SerializationMethod.Json"/>.
+    /// </summary>
+    public class JsonSerializationProvider : SerializationProvider
+    {
+
+        #region Properties
+
+        /// <summary>
+        /// The <see cref="JsonSerializerSettings"/> utilized during the serialization and deserialization process.
+        /// </summary>
+        public JsonSerializerSettings Settings { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Creates a new instance of <see cref="JsonSerializationProvider"/>.
+        /// </summary>
+        public JsonSerializationProvider() : base(SerializationMethod.Json) { }
+
+        #endregion
+
+        #region Public Methods
+
+        public override T Deserialize<T>(string input) => JsonConvert.DeserializeObject<T>(input, Settings);
+        public override string Serialize<T>(T input) => JsonConvert.SerializeObject(input, Settings);
+
+        #endregion
+
+    }
+}
