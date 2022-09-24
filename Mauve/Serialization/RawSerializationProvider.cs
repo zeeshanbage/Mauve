2ec7f3168602887092cc12df5c1c1f13a13bf2a1
tacosontitan <jkdavis1691@gmail.com>
2022-09-24 10:32:48 -0500

    Added a basic raw serialization provider.

---
+++ Mauve/Serialization/RawSerializationProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Mauve.Serialization
+{
+    /// <summary>
+    /// Represents a <see cref="SerializationProvider"/> focused on serializing and deserializing data using <see cref="SerializationMethod.None"/>.
+    /// </summary>
+    internal class RawSerializationProvider : SerializationProvider
+    {
+
+        #region Constructor
+
+        /// <summary>
+        /// Creates a new instance of <see cref="RawSerializationProvider"/>.
+        /// </summary>
+        public RawSerializationProvider() : base(SerializationMethod.Json) { }
+
+        #endregion
+
+        #region Public Methods
+
+        public override T Deserialize<T>(string input) => (T)Convert.ChangeType(input, typeof(T));
+        public override string Serialize<T>(T input) => input.ToString();
+
+        #endregion
+
+    }
+}
