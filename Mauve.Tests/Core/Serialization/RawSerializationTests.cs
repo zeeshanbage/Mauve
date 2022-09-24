a234c049acdfe900dd71842aba1f0902142d6955
tacosontitan <jkdavis1691@gmail.com>
2022-09-24 16:07:00 -0500

    Added raw serialization test shell.

---
+++ Mauve.Tests/Core/Serialization/RawSerializationTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mauve.Tests.Core.Serialization
+{
+    [TestClass]
+    public class RawSerializationTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
