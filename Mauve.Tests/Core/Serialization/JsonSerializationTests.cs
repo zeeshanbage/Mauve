183394ec5ef6cf3d74e137c692cf59eb75a4216b
tacosontitan <jkdavis1691@gmail.com>
2022-09-24 16:06:48 -0500

    Added json serialization test shell.

---
+++ Mauve.Tests/Core/Serialization/JsonSerializationTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mauve.Tests.Core.Serialization
+{
+    [TestClass]
+    public class JsonSerializationTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
