ec3ab851682f47d8d9332c818747c25293453de5
tacosontitan <jkdavis1691@gmail.com>
2022-09-24 10:18:11 -0500

    Created xml serialization provider.

---
+++ Mauve/Serialization/XmlSerializationProvider.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Mauve.Serialization
+{
+    /// <summary>
+    /// Represents a <see cref="SerializationProvider"/> focused on serializing and deserializing data using <see cref="SerializationMethod.Xml"/>.
+    /// </summary>
+    public class XmlSerializationProvider : SerializationProvider
+    {
+
+        #region Constructor
+
+        /// <summary>
+        /// Creates a new instance of <see cref="XmlSerializationProvider"/>.
+        /// </summary>
+        public XmlSerializationProvider() : base(SerializationMethod.Xml) { }
+
+        #endregion
+
+        #region Public Methods
+
+        public override T Deserialize<T>(string input)
+        {
+            using (var stringReader = new StringReader(input))
+            {
+                var settings = new XmlReaderSettings();
+                using (var xmlReader = XmlReader.Create(stringReader, settings))
+                {
+                    var serializer = new XmlSerializer(typeof(T));
+                    return (T)Convert.ChangeType(serializer.Deserialize(xmlReader), typeof(T));
+                }
+            }
+        }
+        public override string Serialize<T>(T input)
+        {
+            using (var textWriter = new StringWriter())
+            {
+                var settings = new XmlWriterSettings() { OmitXmlDeclaration = true, Indent = true, ConformanceLevel = ConformanceLevel.Auto };
+                using (var xmlWriter = XmlWriter.Create(textWriter, settings))
+                {
+                    var serializer = new XmlSerializer(typeof(T));
+                    serializer.Serialize(xmlWriter, input);
+                    return textWriter.ToString();
+                }
+            }
+        }
+
+        #endregion
+
+    }
+}
