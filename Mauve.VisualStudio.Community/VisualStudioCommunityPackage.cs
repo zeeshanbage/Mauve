6a99d8d5ff5b09f63a6711376a55ec9827d6b23e
tacosontitan <jkdavis1691@gmail.com>
2022-09-24 22:42:20 -0500

    Renamed cleanse to standardize

--- Mauve.VisualStudio.Community/VisualStudioCommunityPackage.cs
+++ Mauve.VisualStudio.Community/VisualStudioCommunityPackage.cs
@@ -49,7 +49,7 @@ namespace Mauve.VisualStudio.Community
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
-            await Mauve.VisualStudio.Community.Commands.CleanseAndSave.InitializeAsync(this);
+            await Mauve.VisualStudio.Community.Commands.Standardize.InitializeAsync(this);
         }
 
         #endregion
