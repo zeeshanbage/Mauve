b1acf63d90d8ab903e7437d1e55e19d1b1afdfca
tacosontitan <jkdavis1691@gmail.com>
2022-09-25 07:04:28 -0500

    Added standardization options.

---
+++ Mauve.VisualStudio.Community/Options/StandardizationOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mauve.VisualStudio.Community.Options
+{
+    internal class StandardizationConfiguration
+    {
+        public bool FormatDocument { get; set; }
+        public bool RemoveAndSortUsings { get; set; }
+        public bool RegionalizeContents { get; set; }
+        public bool AlphabetizeContents { get; set; }
+        public bool SaveAll { get; set; }
+    }
+}
