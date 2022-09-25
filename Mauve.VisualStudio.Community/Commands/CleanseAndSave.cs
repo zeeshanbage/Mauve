1a9816ce5f910f56dc00c464564edab05cd91d52
tacosontitan <jkdavis1691@gmail.com>
2022-09-24 22:03:08 -0500

    Updated cleanse and save to use command base.

--- Mauve.VisualStudio.Community/Commands/CleanseAndSave.cs
+++ Mauve.VisualStudio.Community/Commands/CleanseAndSave.cs
@@ -4,6 +4,8 @@ using System.Globalization;
 using System.Threading;
 using System.Threading.Tasks;
 
+using Mauve.VisualStudio.Community.Core;
+
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 
@@ -11,97 +13,35 @@ using Task = System.Threading.Tasks.Task;
 
 namespace Mauve.VisualStudio.Community.Commands
 {
-    /// <summary>
-    /// Command handler
-    /// </summary>
-    internal sealed class CleanseAndSave
+    internal sealed class CleanseAndSave : CommandBase<CleanseAndSave>
     {
-        /// <summary>
-        /// Command ID.
-        /// </summary>
-        public const int CommandId = 0x0100;
 
-        /// <summary>
-        /// Command menu group (command set GUID).
-        /// </summary>
-        public static readonly Guid CommandSet = new Guid("7a54043a-2ca9-4ec1-8313-712c01c181ad");
+        #region Constructor
 
-        /// <summary>
-        /// VS Package that provides this command, not null.
-        /// </summary>
-        private readonly AsyncPackage package;
+        public CleanseAndSave(AsyncPackage package, OleMenuCommandService commandService) :
+            base(0x0100, new Guid("7a54043a-2ca9-4ec1-8313-712c01c181ad"), package, commandService)
+        { }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="CleanseAndSave"/> class.
-        /// Adds our command handlers for menu (commands must exist in the command table file)
-        /// </summary>
-        /// <param name="package">Owner package, not null.</param>
-        /// <param name="commandService">Command service to add command to, not null.</param>
-        private CleanseAndSave(AsyncPackage package, OleMenuCommandService commandService)
+        #endregion
+
+        #region Protected Methods
+
+        protected override void Run()
         {
-            this.package = package ?? throw new ArgumentNullException(nameof(package));
-            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
-
-            var menuCommandID = new CommandID(CommandSet, CommandId);
-            var menuItem = new MenuCommand(this.Execute, menuCommandID);
-            commandService.AddCommand(menuItem);
-        }
-
-        /// <summary>
-        /// Gets the instance of the command.
-        /// </summary>
-        public static CleanseAndSave Instance
-        {
-            get;
-            private set;
-        }
-
-        /// <summary>
-        /// Gets the service provider from the owner package.
-        /// </summary>
-        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
-        {
-            get
-            {
-                return this.package;
-            }
-        }
-
-        /// <summary>
-        /// Initializes the singleton instance of the command.
-        /// </summary>
-        /// <param name="package">Owner package, not null.</param>
-        public static async Task InitializeAsync(AsyncPackage package)
-        {
-            // Switch to the main thread - the call to AddCommand in CleanseAndSave's constructor requires
-            // the UI thread.
-            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
-
-            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
-            Instance = new CleanseAndSave(package, commandService);
-        }
-
-        /// <summary>
-        /// This function is the callback used to execute the command when the menu item is clicked.
-        /// See the constructor to see how the menu item is associated with this function using
-        /// OleMenuCommandService service and MenuCommand class.
-        /// </summary>
-        /// <param name="sender">Event sender.</param>
-        /// <param name="e">Event args.</param>
-        private void Execute(object sender, EventArgs e)
-        {
-            ThreadHelper.ThrowIfNotOnUIThread();
-            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
-            string title = "CleanseAndSave";
+            string message = "Hello World!";
+            string title = "Mauve";
 
             // Show a message box to prove we were here
-            VsShellUtilities.ShowMessageBox(
-                this.package,
+            _ = VsShellUtilities.ShowMessageBox(
+                Package,
                 message,
                 title,
                 OLEMSGICON.OLEMSGICON_INFO,
                 OLEMSGBUTTON.OLEMSGBUTTON_OK,
                 OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
         }
+
+        #endregion
+
     }
 }
