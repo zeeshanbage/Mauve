d213f7accfb49e9ca85c3f63be592ad964bcec61
tacosontitan <jkdavis1691@gmail.com>
2022-09-25 14:15:02 -0500

    Added property parser

---
+++ Mauve.VisualStudio.Community/Text/DocumentParser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using EnvDTE;
+
+namespace Mauve.VisualStudio.Community.Text
+{
+    internal abstract class DocumentParser<T>
+    {
+
+        #region Properties
+
+        protected Document Document { get; private set; }
+
+        #endregion
+
+        #region Constructor
+
+        public DocumentParser(Document document) => Document = document;
+
+        #endregion
+
+        #region Public Methods
+
+        public abstract T Parse();
+
+        #endregion
+
+    }
+}
---
+++ Mauve.VisualStudio.Community/Text/DocumentPropertyParser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using EnvDTE;
+
+namespace Mauve.VisualStudio.Community.Text
+{
+    internal class DocumentPropertyParser : DocumentParser<IEnumerable<string>>
+    {
+        public DocumentPropertyParser(Document document) : base(document)
+        {
+        }
+
+        public override IEnumerable<string> Parse() => throw new NotImplementedException();
+    }
+}
