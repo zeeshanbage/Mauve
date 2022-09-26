1c2ab921775d81212b9c8d8b4010610c3a14502e
tacosontitan <jkdavis1691@gmail.com>
2022-09-25 21:11:33 -0500

    Added serialization tests.

---
+++ Mauve.Tests/Core/Extensibility/GenericExtensionTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Mauve.Extensibility;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mauve.Tests.Core.Extensibility
+{
+    [TestClass]
+    public class GenericExtensionTests
+    {
+        [TestMethod]
+        [DataRow(32, SerializationMethod.None)]
+        [DataRow(32, SerializationMethod.Binary)]
+        [DataRow(32, SerializationMethod.Xml)]
+        [DataRow(32, SerializationMethod.Json)]
+        public void SerializeAndDeserialize(int input, SerializationMethod serializationMethod)
+        {
+            string serializedInput = input.Serialize(serializationMethod);
+            int deserializedValue = serializedInput.Deserialize<int>(serializationMethod);
+            Assert.AreEqual(input, deserializedValue);
+        }
+    }
+}
