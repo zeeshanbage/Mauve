ba4f5337cc4da3a52ab23329b129466433d278e0
tacosontitan <jkdavis1691@gmail.com>
2022-09-25 20:47:27 -0500

    Added constrain method.

--- Mauve/Extensibility/IComparableExtensions.cs
+++ Mauve/Extensibility/IComparableExtensions.cs
@@ -8,6 +8,23 @@ namespace Mauve.Extensibility
     public static class IComparableExtensions
     {
         /// <summary>
+        /// Constrains an input value to a specified range.
+        /// </summary>
+        /// <typeparam name="T">The type of data to constrain.</typeparam>
+        /// <param name="input">The input value to constrain.</param>
+        /// <param name="lowerBound">The lower bound of the constraint.</param>
+        /// <param name="upperBound">The upper bound of the constraint.</param>
+        /// <returns>Returns the input if it falls on or between the lower and upper bounds, otherwise the exceeded bound is returned in its place.</returns>
+        public static T Constrain<T>(this T input, T lowerBound, T upperBound) where T : IComparable
+        {
+            if (input.CompareTo(lowerBound) < 0)
+                input = lowerBound;
+            else if (input.CompareTo(upperBound) > 0)
+                input = upperBound;
+
+            return input;
+        }
+        /// <summary>
         /// Determines if the calling object's value is equal to or greater than the specified minimum value, while remaining less than or equal to the specified maximum value.
         /// </summary>
         /// <param name="value">The calling object's value.</param>
