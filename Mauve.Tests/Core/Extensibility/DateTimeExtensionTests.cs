9d0514c2ddaf22e0c2e3747e3561287fc732dd56
tacosontitan <jkdavis1691@gmail.com>
2022-09-25 20:40:14 -0500

    Created common format test.

--- Mauve.Tests/Core/Extensibility/DateTimeExtensionTests.cs
+++ Mauve.Tests/Core/Extensibility/DateTimeExtensionTests.cs
@@ -10,8 +10,11 @@ namespace Mauve.Tests.Core.Extensibility
     [TestClass]
     public class DateTimeExtensionTests
     {
-        [TestMethod("ISO8601")]
-        public void Iso8601()
+        [TestMethod("Test Common Formats")]
+        [DataRow(DateFormat.Iso8601, 2)]
+        [DataRow(DateFormat.Rfc3339, 3)]
+        [DataRow(DateFormat.MsSql, 2)]
+        public void CommonFormatTest(DateFormat format, int millisecondLength)
         {
             DateTime now = DateTime.Now;
             int year = now.Year;
@@ -20,64 +23,29 @@ namespace Mauve.Tests.Core.Extensibility
             int hour = now.Hour;
             int minute = now.Minute;
             int second = now.Second;
-            string millisecond = now.Millisecond.ToString().Substring(0, 2);
-            string nowIso8601 = now.ToString(DateFormat.Iso8601);
+            string millisecond = now.Millisecond.ToString().Substring(0, millisecondLength);
+            string nowFormatted = now.ToString(format);
 
             // Use SubstringQueue to quickly parse the individual chunks.
-            // yyyy-MM-ddTHH:mm:ss.ffK
-            var queue = new SubstringQueue(nowIso8601);
+            // ISO8601: yyyy-MM-ddTHH:mm:ss.ffK
+            // RFC3339: yyyy-MM-dd'T'HH:mm:ss.fffK
+            // MSSQL:   yyyy-MM-dd HH:mm:ss.fff
+            //
+            // During testing, RFC3339's 'T' value was presented as simply T which allowed to be considered a common format.
+            var queue = new SubstringQueue(nowFormatted);
             queue.Next(4, out int parsedYear)
                  .Skip(1)
                  .Next(2, out int parsedMonth)
                  .Skip(1)
                  .Next(2, out int parsedDay)
-                 .Skip(1)
+                 .Skip(1) // 
                  .Next(2, out int parsedHour)
                  .Skip(1)
                  .Next(2, out int parsedMinute)
                  .Skip(1)
                  .Next(2, out int parsedSecond)
                  .Skip(1)
-                 .Next(2, out string parsedMillisecond)
-                 .Remainder(out string _);
-
-            Assert.AreEqual(year, parsedYear);
-            Assert.AreEqual(month, parsedMonth);
-            Assert.AreEqual(day, parsedDay);
-            Assert.AreEqual(hour, parsedHour);
-            Assert.AreEqual(minute, parsedMinute);
-            Assert.AreEqual(second, parsedSecond);
-            Assert.AreEqual(millisecond, parsedMillisecond);
-        }
-        [TestMethod("RFC3339")]
-        public void Rfc3339()
-        {
-            DateTime now = DateTime.Now;
-            int year = now.Year;
-            int month = now.Month;
-            int day = now.Day;
-            int hour = now.Hour;
-            int minute = now.Minute;
-            int second = now.Second;
-            string millisecond = now.Millisecond.ToString().Substring(0, 3);
-            string nowRfc3339 = now.ToString(DateFormat.Rfc3339);
-
-            // Use SubstringQueue to quickly parse the individual chunks.
-            // yyyy-MM-dd'T'HH:mm:ss.fffK
-            var queue = new SubstringQueue(nowRfc3339);
-            queue.Next(4, out int parsedYear)
-                 .Skip(1)
-                 .Next(2, out int parsedMonth)
-                 .Skip(1)
-                 .Next(2, out int parsedDay)
-                 .Skip(1) // The 'T' is somehow written as just T.
-                 .Next(2, out int parsedHour)
-                 .Skip(1)
-                 .Next(2, out int parsedMinute)
-                 .Skip(1)
-                 .Next(2, out int parsedSecond)
-                 .Skip(1)
-                 .Next(3, out string parsedMillisecond)
+                 .Next(millisecondLength, out string parsedMillisecond)
                  .Remainder(out string _);
 
             Assert.AreEqual(year, parsedYear);
